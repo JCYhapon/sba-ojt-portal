cb95b043eabb89017848d5954a54374034f3a1c1
JCYhapon <yhaponjc@gmail.com>
2024-02-08 22:36:37 +0800

    redesigned layout

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -80,20 +80,21 @@
 
         <div class="grid grid-rows-3 lg:max-h-[70vh]  xs:max-h-screen xs:gap-8 gap-0 h-auto">
             <!--  FIRST ROW -->
-            <div class="bg-white row-span-1 grid grid-cols-3 x p-8 shadow-md rounded-md h-48 py-12">
-                <div class="flex flex-col justify-between">
-                    <div class="flex flex-row gap-10">
+            <div class="bg-white row-span-1 flex lg:flex-row md:flex-row lg:justify-between xs:flex-col  p-8 shadow-md rounded-md lg:h-48 h-auto py-12">
+                <div class="flex flex-col gap-2">
+                    <div class="flex flex-row gap-2">
                         <h1 class="text-3xl">{{ $student->lastName }} {{ $student->firstName }}</h1>
                     </div>
                     <div>
                         <p class="text-lg capitalize"><span class="font-semibold">Section:</span> {{ $student->section }}</p>
                     </div>
-
-                </div>
-                <div class="flex items-end">
                     <div>
                         <p class="text-lg"><span class="font-semibold">Email:</span> {{ $student->email }}</p>
                     </div>
+                </div>
+
+                <div class="flex items-center">
+
                 </div>
                 <div class="flex flex-col justify-between">
                     <p class="text-lg"><span class="font-semibold">Status:</span> {{ $student->status == 1 ? 'Active' : ($student->status == 2 ? 'Drop' : 'Unknown') }}</p>
