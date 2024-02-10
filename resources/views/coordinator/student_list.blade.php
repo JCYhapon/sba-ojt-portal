3c5ee27420f23d3edda1b47aa7cf82bf8c4c5840
JCYhapon <yhaponjc@gmail.com>
2024-02-10 23:47:32 +0800

    responsive page

--- resources/views/coordinator/student_list.blade.php
+++ resources/views/coordinator/student_list.blade.php
@@ -123,7 +123,7 @@
             <div class="flex flex-col justify-between min-h-70vh ">
 
                 <!-- Students Table -->
-                <div>
+                <div class="overflow-x-auto">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
