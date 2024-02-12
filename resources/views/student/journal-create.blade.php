d29480b47b00a9181730e8d802682fc127e2f878
JCYhapon <yhaponjc@gmail.com>
2024-02-12 22:34:04 +0800

    removed unused codes

--- resources/views/student/journal-create.blade.php
+++ resources/views/student/journal-create.blade.php
@@ -90,10 +90,7 @@
 <<<<<<< HEAD
 
 =======
-                <!-- Back Button -->
-                <button class="mb-8">
-                    <a href="{{ url()->previous() }}"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMNJREFUSEvtlDEKwkAQRV8OIWiv4BlE8BaCteB1xFrwMArewcZe8BD6wYUtss5Mku2SMizv/fnZSUPlp6nMZxSYDUcqmgI74GhSswNegeBXYAEcgLNX4hEIfgPmwBNYAa+hBDn8AWwicIX4N8EEuP+SC74G3t7k6VxJILg6X34/bGd4aYIcHgncGrbtZXWBUletKNUyiMTag943yRJoml674BEkSfpV7IGL93p5BeLNgC1w8sKtTY5wimcjE3QSjgKztg/ExiAZuzHo1gAAAABJRU5ErkJggg==" /></a>
-                </button>
+
 >>>>>>> b564f6e9861e90319acf17a2267f9788383b1763
                 <div class="flex flex-row justify-between mb-[1.5rem]">
 
