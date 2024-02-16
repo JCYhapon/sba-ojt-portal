2a31647b8e6afac8aec9956c4dc44c87d45bf300
JCYhapon <yhaponjc@gmail.com>
2024-02-16 21:32:51 +0800

    added border for text area

--- resources/views/coordinator/company_list-create.blade.php
+++ resources/views/coordinator/company_list-create.blade.php
@@ -83,17 +83,17 @@
           <div class="flex flex-col gap-6">
             <div class="flex flex-col">
               <label for="name" class="text-sm font-semibold">Name</label>
-              <input type="text" name="name" placeholder="Company's name" class="p-1 rounded-sm border-black border-[2px]" />
+              <input type="text" name="name" placeholder="Company's name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
             </div>
 
             <div class="flex flex-col">
               <label for="email" class="text-sm font-semibold">Email</label>
-              <input type="email" name="email" placeholder="Company's email" class="p-1 rounded-sm border-black border-[2px]" />
+              <input type="email" name="email" placeholder="Company's email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
             </div>
 
             <div class="flex flex-col">
               <label for="address" class="text-sm font-semibold">Address</label>
-              <input type="text" name="address" placeholder="Company's address" class="p-1 rounded-sm border-black border-[2px]" />
+              <input type="text" name="address" placeholder="Company's address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
             </div>
 
             <div class="flex items-center space-x-4">
