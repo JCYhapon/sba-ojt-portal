b03ea3fc4a6c69f6080b2f8045ef47af6a8a939d
JCYhapon <yhaponjc@gmail.com>
2024-02-21 20:11:04 +0800

    modified

--- resources/views/student/journal-create.blade.php
+++ resources/views/student/journal-create.blade.php
@@ -122,7 +122,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="supervisorSignature" class=" block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Supervisor Signature</label>
+                    <label for="supervisorSignature" class=" block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Documentation</label>
                     <input type="file" name="supervisorSignature" accept="image/*" class="form-control-file block w-[30rem] text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
                 </div>
 
