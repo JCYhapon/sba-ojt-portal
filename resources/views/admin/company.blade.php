6c44efca9bd25107176a4ed54fdac3edc47225db
JCYhapon <yhaponjc@gmail.com>
2024-02-25 17:27:45 +0800

    modified: added  the function and layout

--- resources/views/admin/company.blade.php
+++ resources/views/admin/company.blade.php
@@ -52,7 +52,6 @@
               <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                 @csrf
               </form>
-
               </a>
             </div>
           </div>
@@ -84,17 +83,6 @@
               <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
             </div>
           </form>
-        </div>
-
-        <div class="flex align-middle justify-center">
-          <a href="{{ route('coordinator.company_create') }}" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="bg-gray-800 text-white px-4 py-2 rounded-xl hover:bg-gray-600 text-sm">
-            <button type="button" id="createProductModalButton" data-modal-target="createProductModal" data-modal-toggle="createProductModal" class="flex items-center justify-center text-white   font-medium rounded-lg text-sm px-2 py-0 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-              <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-              </svg>
-              New Company
-            </button>
-          </a>
         </div>
       </div>
 
