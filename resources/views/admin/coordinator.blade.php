28fdd3543661672df76caa6345bebebc3b6d3d69
JCYhapon <yhaponjc@gmail.com>
2024-02-25 17:28:48 +0800

    modified: added  the function and layout

--- resources/views/admin/coordinator.blade.php
+++ resources/views/admin/coordinator.blade.php
@@ -15,6 +15,9 @@
   <!-- Custom CSS -->
   <link rel="stylesheet" href="{{ asset('css/background.css') }}">
 
+  <!-- FLOWBITE CDN -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 
   <title>Coordinator</title>
 </head>
@@ -89,7 +92,7 @@
         </div>
 
         <div class="flex align-middle justify-center">
-          <a href="#" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="bg-gray-800 text-white px-4 py-2 rounded-xl hover:bg-gray-600 text-sm">
+          <a href="{{ route('admin-coordinator_create') }}" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="bg-gray-800 text-white px-4 py-2 rounded-xl hover:bg-gray-600 text-sm">
             <button type="button" id="createProductModalButton" data-modal-target="createProductModal" data-modal-toggle="createProductModal" class="flex items-center justify-center text-white   font-medium rounded-lg text-sm px-2 py-0 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
               <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
@@ -103,7 +106,7 @@
 
       <div class="flex flex-col justify-between min-h-[70vh] overflow-x-auto">
 
-        <!-- Students Table -->
+        <!-- Coordinators Table -->
         <div class="">
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 overflow-x-auto">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -126,7 +129,7 @@
 
                 <td class="py-2 px-4 border-b">
                   <div class="flex gap-3">
-                    <a href="{{ route('coordinator_student-list.edit',['students' => $user]) }}">
+                    <a href="{{ route('admin-coordinator_edit', $user->id) }}">
                       <button data-modal-target="default-modal" data-modal-toggle="default-modal" type="button">
                         <box-icon name='edit' color='#1f2937'></box-icon>
                       </button>
