0b4605d85d73e45fe98f89074abcce2caab99a77
JCYhapon <yhaponjc@gmail.com>
2024-02-26 23:31:25 +0800

    student id to coordinator id

--- resources/views/admin/coordinator_create.blade.php
+++ resources/views/admin/coordinator_create.blade.php
@@ -79,7 +79,7 @@
                     <div class="flex flex-col gap-6">
                         {{-- Student ID / id --}}
                         <div class="col-span-2">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Student ID</label>
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Coordinator ID</label>
                             <input type="text" name="studentID" id="studentID" maxlength="8" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required="" placeholder="Student ID">
                         </div>
                         {{-- First Name --}}
