b550d10773a4142beef6c0f16d9e06acbf9ce013
JCYhapon <yhaponjc@gmail.com>
2024-02-27 14:38:20 +0800

    modified: responsive page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,18 +14,15 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/loginpage.css') }}">
 
-
-
-
     <title>SBA-OJT PORTAL</title>
 
 </head>
 
 <body>
     <section class="min-h-screen flex justify-center items-center text-white ">
-        <div class="w-full flex">
+        <div class="w-full flex flex-row">
 
-            <div class="w-[100%]">
+            <div class="lg:w-[100%] md:w-[70%]  md:hidden ssm:hidden sm:hidden ss:hidden xs:hidden lg:block llg:block">
                 <div id="background" class="w-[100%] h-full ">
 
                 </div>
@@ -33,27 +30,25 @@
 
             <!-- Laravel login form -->
 
-            <div id="form" class="w-[80%] h-[100vh] mx-auto  p-[10rem] flex flex-col  gap-[5rem]">
-                <div class="flex flex-row justify-center">
-                    <img src="{{ asset('assets/logo.png') }}" alt="" class="">
+            <div id="form" class="w-[80%] md:[100%] lg:w-[70%] h-[100vh] mx-auto  lg:p-[10rem] flex flex-col  gap-[3rem] items-center">
+                <div class="flex flex-row justify-center ">
+                    <img src="{{ asset('assets/sba-logo.png') }}" alt="sba-logo" class="">
                 </div>
 
-                <div class="bg-white p-6">
-                    <h1 class="text-center mb-5">
-                        <span class="font-bold text-3xl text-black ">SBA OJT Portal</span>
+                <div class="bg-white h-auto p-[3rem] w-[100%] llg:w-[70%] lg:w-[100%] mmd:w-[100%] md:w-[100%]  rounded-[6%]">
+                    <h1 class="text-center mb-[30px]">
+                        <span class="font-bold text-2xl text-black ">SBA OJT Portal</span>
                     </h1>
                     <form method="POST" action="{{ route('login') }}" class="mb-4">
                         @csrf
                         <div class="mb-4">
-                            <label for="email" class="text-sm font-medium text-black">Email Address</label>
-                            <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="Enter your email" required autofocus class="w-full px-4 py-2 border rounded-md text-black bg-gray-100 focus:border-black focus:outline-none focus:shadow-outline-blue @error('email') border-red-500 @enderror">
+                            <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus class="w-full px-4 py-2 border rounded-md text-black bg-gray-100 focus:border-black focus:outline-none focus:shadow-outline-blue @error('email') border-red-500 @enderror">
                             @error('email')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
                         </div>
                         <div class="mb-4">
-                            <label for="password" class="text-sm font-medium text-black">Password</label>
-                            <input id="password" type="password" placeholder="Entern your password" name="password" required class="w-full px-4 py-2 border rounded-md text-black bg-gray-100 focus:border-black focus:outline-none focus:shadow-outline-blue @error('password') border-red-500 @enderror">
+                            <input id="password" type="password" placeholder="Password" name="password" required class="w-full px-4 py-2 border rounded-md text-black bg-gray-100 focus:border-black focus:outline-none focus:shadow-outline-blue @error('password') border-red-500 @enderror">
                             @error('password')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
@@ -64,8 +59,8 @@
                                 <span class="ml-2 text-sm text-gray-500">Remember me</span>
                             </label>
                         </div>
-                        <button type="submit" class="w-full bg-gray-800 text-gray-200 p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:shadow-outline-blue">
-                            Log in
+                        <button type="submit" class="w-full font-semibold bg-[#AD974F] text-white p-2 rounded-md hover:bg-[#736023] focus:outline-none focus:shadow-outline-blue">
+                            Login
                         </button>
                     </form>
                 </div>
