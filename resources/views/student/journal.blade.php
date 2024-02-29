db4abbab2db510645a294700c7c1c056c14f19c0
JCYhapon <yhaponjc@gmail.com>
2024-02-29 20:24:02 +0800

    button changed color

--- resources/views/student/journal.blade.php
+++ resources/views/student/journal.blade.php
@@ -87,7 +87,7 @@
                 @endphp
 
                 @if(!$disableCreateJournal)
-                <a href="{{ route('create_journal') }}" class="bg-gray-800 text-white px-4 py-2 rounded-xl hover:bg-gray-600 text-sm">
+                <a href="{{ route('create_journal') }}" class="bg-[#AD974F] text-white px-4 py-2 rounded-xl hover:bg-gray-600 text-sm">
                     <p type="button" class="flex items-center justify-center text-white font-medium rounded-lg text-sm px-2 py-0 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                         <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
@@ -129,7 +129,7 @@
                                 <td class="py-2 px-4 border-b">
                                     {{-- <a href="{{ route('edit_journal',['journalID' => $journal->journalID]) }}"> --}}
                                     <a href="{{ route('edit_journal', ['journal' => $journal->journalID]) }}">
-                                        <box-icon name='edit' color='#1f2937' style='font-size: 45px;'></box-icon>
+                                        <box-icon name='edit' color='#AD974F' style='font-size: 45px;'></box-icon>
                                     </a>
 
                                 </td>
