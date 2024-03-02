029576450938b38179bccb94b50b96b63d537cb2
JCYhapon <yhaponjc@gmail.com>
2024-03-02 13:41:33 +0800

    new page: forgot password

---
+++ resources/views/auth/forgotpassword.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Tailwindcss CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/loginpage.css') }}">
+
+    <title>SBA-OJT PORTAL</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
