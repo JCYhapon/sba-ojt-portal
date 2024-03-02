0423ec6183b95b0d1ad38bc750df29c495b3744f
JCYhapon <yhaponjc@gmail.com>
2024-03-02 23:14:04 +0800

    modified

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,6 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 |
 */
 
-//FORGOT PASSWORD
-Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
-
 //LANDING PAGE
 Route::get('/', function () {
     return view('auth.login');
