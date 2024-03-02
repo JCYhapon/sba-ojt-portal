e8c10af8714399eb16e571c8967f0c1cc78efb50
JCYhapon <yhaponjc@gmail.com>
2024-03-02 23:15:12 +0800

    added logout for new tab

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,37 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{----alphinejs----}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.13.3/cdn.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/background.css') }}">
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <!-- Tailwindcss CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Profile</title>
+
+
+</head>
+
+<body>
+
+    <div>
+        <a class="" href="{{ route('logout') }}" onclick="event.preventDefault();
+       document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
     </div>
-</div>
-@endsection
+
+</body>
+
+</html>
