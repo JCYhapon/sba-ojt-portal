e61aad2e62f0228b3b2e27a86e1d9362c0f44729
JCYhapon <yhaponjc@gmail.com>
2024-03-03 16:59:49 +0800

    new page layout with confirming password

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,59 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{----alphinejs----}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.13.3/cdn.js"></script>
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+    <!-- Tailwindcss CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/loginpage.css') }}">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+    <title>SBA-OJT PORTAL</title>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+</head>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body class="bg-gray-800">
+    <section class="min-h-screen flex justify-center items-center mx-auto text-white">
+        <div class="w-full py-6">
+            <div class="w-80 mx-auto bg-white p-8 shadow-lg rounded-md">
+                <h1 class="text-center mb-5">
+                    <span class="font-bold text-3xl text-black">SBA OJT Portal</span>
+                </h1>
+                <form method="POST" action="{{ route('reset.password.post') }}">
+                    @csrf
+                    <input type="text" hidden name="token" value="{{ $token }}">
+                    <span class="block text-center text-gray-600 mt-4 pb-5">Create new password</span>
+                    <div class="mb-4">
+                        <label for="email" class="block text-sm font-medium text-gray-700">Email Address</label>
+                        <input id="email" type="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md text-gray-700" name="email" required>
+                    </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
+                    <div class="mb-4">
+                        <label for="password" class="block text-sm font-medium text-gray-700">Enter New Password</label>
+                        <input id="password" type="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md text-gray-700" name="password" required autofocus>
+                    </div>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="mb-4">
+                        <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm New Password</label>
+                        <input id="password_confirmation" type="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md text-gray-700" name="password_confirmation" required autofocus>
+                    </div>
+
+                    <div class="mb-4">
+                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                            Submit
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </section>
+</body>
+
+
+</html>
