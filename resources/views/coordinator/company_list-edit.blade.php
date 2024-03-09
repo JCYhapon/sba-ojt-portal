caceec6448e84c0cd47271f5d76ef04cf278c787
JCYhapon <yhaponjc@gmail.com>
2024-03-09 15:28:31 +0800

    modified code

--- resources/views/coordinator/company_list-edit.blade.php
+++ resources/views/coordinator/company_list-edit.blade.php
@@ -111,7 +111,7 @@
                     <div class="sm:col-span-2">
                         <label for="workType" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Work Type Offered:</label>
                         <select name="workType" id="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option value="" disabled {{ empty($company->workType) ? 'selected' : '' }}></option>
+                            <option value="" disabled {{ empty($company->workType) ? 'selected' : '' }}>Choose Work Type Offered</option>
                             <option value="1" {{ $company->workType == 1 ? 'selected' : '' }}>On Site</option>
                             <option value="2" {{ $company->workType == 2 ? 'selected' : '' }}>From Home</option>
                             <option value="3" {{ $company->workType == 3 ? 'selected' : '' }}>Hybrid</option>
@@ -141,8 +141,8 @@
                             'Management Accounting',
                             'Financial Management',
                             'Human Resource Management',
-                            'Marketing Managements ',
-                            'Legal Managements'
+                            'Marketing Management',
+                            'Legal Management'
                             ];
 
                             // Remove positions that are already selected
@@ -167,7 +167,7 @@
                             <li>
                                 @endif
 
-                                <select name="position" id="addPosition" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-[25vh] p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                <select name="position" id="addPosition" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-[25vh] p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 mr-2">
                                     <option value="">Choose a position</option>
                                     @foreach($availablePositions as $availablePosition)
                                     <option value="{{ $availablePosition }}">{{ $availablePosition }}</option>
