c92a64c9dd4063ab1f16209e561ac43d3cf3b1e8
JCYhapon <yhaponjc@gmail.com>
2024-03-12 17:24:22 +0800

    new layout

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -12,9 +12,12 @@
     <!-- Tailwindcss CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
 
+    <!-- Boxicon CDN -->
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/background.css') }}">
-
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <title>Dashboard</title>
 </head>
 
@@ -68,21 +71,21 @@
     </div>
     <!-- END OF NAVBAR -->
 
-    <div class="w-full lg:container mx-auto lg:max-w-screen-xl  mt-8 p-12 h-auto">
+    <div class="w-full lg:container mx-auto lg:max-w-screen-xl  mt-4 lg:p-12 p-4 h-auto">
         @foreach (explode(',',Auth::user()->schoolID) as $studentID)
         @php
         $student = \App\Models\Student::where('studentID', $studentID)->first();
         @endphp
 
-        <div class="flex flex-col gap-8">
+        <div class="flex flex-col gap-8 h-auto">
             <!--  FIRST ROW -->
-            <div class=" flex justify-between  h-[30rem]">
-                <div class="flex flex-col gap-8 bg-white p-6 w-[68%] shadow-md rounded-md">
+            <div class=" flex lg:flex-row flex-col justify-between  ">
+                <div class="flex flex-col gap-8 bg-white p-6 lg:w-[68%] shadow-md rounded-md" data-aos="fade-right" data-aos-delay="600" data-aos-duration="1000">
                     <div class="grid lg:gap-2 md:gap-0 ss:gap-0 border-b">
-                        <div class="flex justify-between">
+                        <div class="flex justify-between lg:flex-row flex-col">
                             <h1 class="lg:text-3xl md:text-[1.5em] sm:text-[1.5em] ss:text-[18px] xs:text-[18px]"><span class="font-bold text-[#AD974F]">Welcome,</span> <span class="capitalize"> {{$student->firstName }}</span> 👋</h1>
                             <div>
-                                <a href=" {{ route('student_profile') }}"><button class="bg-[#AD974F] text-white p-1 rounded-md lg:text-sm lg:w-40 md:text-[14px] md:w-[9rem] sm:text-[13px] sm:w-[9rem] ss:text-[13px] ss:w-[9rem] xs:text-[13px] xs:w-[9rem]">Go to your
+                                <a href=" {{ route('student_profile') }}"><button class="bg-[#AD974F] hover:bg-[#736023] text-white text-sm p-1 rounded-md lg:text-sm lg:w-40 md:text-[14px] md:w-[9rem] sm:text-[13px] sm:w-[9rem] ss:text-[13px] ss:w-[9rem] xs:text-[13px] xs:w-[9rem]">Go to your
                                         Profile</button></a>
                             </div>
                         </div>
@@ -95,11 +98,43 @@
                         </p>
                     </div>
 
-                    <div>
-                        <h1 class="lg:text-2xl md:text-lg sm:text-lg ss:text-lg font-semibold">View Partner Companies Here:</h1>
+                    <div class="flex gap-6">
+                        <h1 class="lg:text-xl md:text-lg sm:text-lg ss:text-lg font-semibold">View Partner Companies Here</h1>
+                        <div>
+                            <button class="bg-[#AD974F] hover:bg-[#736023] text-white p-1 rounded-md lg:text-sm lg:w-36 md:text-[14px] md:w-[9rem] sm:text-[14px] sm:w-[9rem] ss:text-[14px] ss:w-[9rem] xs:text-[14px] xs:w-[9rem]">
+                                <a href="{{ route('student_company-list') }}">Company List</a></button>
+                        </div>
                     </div>
-                    <div><button class="bg-[#AD974F] text-white p-1 rounded-md lg:text-sm lg:w-36 md:text-[14px] md:w-[9rem] sm:text-[14px] sm:w-[9rem] ss:text-[14px] ss:w-[9rem] xs:text-[14px] xs:w-[9rem]">
-                            <a href="{{ route('student_company-list') }}">Company List</a></button></div>
+
+                    <h2 class="mt-8 text-lg font-semibold text-[#AD974F]">TITLE</h2>
+                    <div class="grid grid-cols-2 ">
+                        <div class="flex flex-col gap-5">
+                            <h2 class="border-b w-[80%]">Coordinator Contact Details</h2>
+                            <div class="ml-3 flex flex-col gap-1">
+                                <div>
+                                    @if ($major === 'Accounting')
+                                    <span class="flex items-center gap-1"> <box-icon name='envelope'></box-icon>rgsantos@hau.edu.ph</span>
+                                    @elseif ($major === 'Management')
+                                    <span class="flex items-center gap-1"> <box-icon name='envelope'></box-icon>mestralla@hau.edu.ph</span>
+                                    @endif
+                                </div>
+                                <div>
+                                    <span class="flex items-center gap-1"><box-icon name='phone'></box-icon>(1234) 12351</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col gap-5">
+                            <h2 class="border-b w-[80%]">SBA Social Page</h2>
+                            <div class="flex flex-col ml-3 gap-1">
+                                <span class="underline">
+                                    <a href="" class="flex gap-1"><box-icon name='facebook-square' type='logo'></box-icon>SBA Facebook Link</a>
+                                </span>
+                                <span class="underline">
+                                    <a href="" class="flex gap-1"><box-icon name='facebook-square' type='logo'></box-icon>SBA Student Council Facebook Link</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="flex flex-col gap-6 bg-white p-6 items-center shadow-lg rounded-md">
@@ -121,50 +156,49 @@
                 </div>
             </div>
 
-            <div class="lg:row-span-3">
-                <div class="grid grid-cols-2 md:gap-10 gap-2">
-                    <div class="bg-white shadow-md rounded-md h-auto flex flex-col items-center justify-center gap-6">
-                        <div>
-                            <h1 class="font-semibold lg:text-2xl md:text-2xl sm:text-xl ss:text-2xl xs:text-xl text-[13px] mt-5">Journal Entry</h1>
-                        </div>
-                        <div>
-                            <a href="{{ route('student_journal') }}"><button class="bg-[#AD974F] text-white p-1 rounded-md lg:text-sm md:text-sm ss:text-sm sm:text-sm xs:text-sm text-[12px] lg:mb-4 w-[6rem] ">Journal</button>
-                        </div></a>
-                        <div>
-                            <p class="lg:text-sm md:text-[13px] sm:text-[12px] mb-2 text-center ss:text-[12px] xs:text-[10px] text-[10px]">Please submit your Daily Journal entry here and input your working hours
-                            </p>
-                        </div>
+
+            <div class="lg:grid lg:grid-cols-2 md:gap-10 gap-2">
+                <div class="bg-white shadow-md rounded-md h-auto flex flex-col items-center justify-center gap-6" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
+                    <div>
+                        <h1 class="font-semibold lg:text-2xl md:text-2xl sm:text-xl ss:text-2xl xs:text-xl text-[13px] mt-5">Journal Entry</h1>
                     </div>
-                    <div class="bg-white shadow-md rounded-md lg:h-48 flex flex-col items-center justify-center gap-6">
-                        <div>
-                            <h1 class="font-semibold lg:text-2xl md:text-2xl sm:text-2xl ss:text-2xl xs:text-xl text-[13px] mt-5">Company Matches</h1>
-                        </div>
-                        <div>
-                            <button class="bg-[#AD974F] text-white p-1 rounded-md lg:text-sm md:text-sm ss:text-sm sm:text-sm xs:text-sm text-[12px] lg:w-36 lg:mb-4 md:w-[9rem]  sm:w-[9rem]  ss:w-[9rem] w-[6rem]"><a href="{{ route('match-students') }}">Matched Companies</a></button>
-                        </div>
-                        <div>
-                            <p class="lg:text-sm md:text-[13px] sm:text-[12px] ss:text-[12px] xs:text-[10px] text-[10px] mb-2 text-center">Contact your coordinator for more information about your company matches
-                            </p>
-                        </div>
+                    <div>
+                        <a href="{{ route('student_journal') }}"><button class="bg-[#AD974F] hover:bg-[#736023] text-white p-1 rounded-md lg:text-sm md:text-sm ss:text-sm sm:text-sm xs:text-sm text-[12px] lg:mb-4 w-[6rem] ">Journal</button>
+                    </div></a>
+                    <div>
+                        <p class="lg:text-sm md:text-[13px] sm:text-[12px] mb-2 text-center ss:text-[12px] xs:text-[10px] text-[10px]">Please submit your Daily Journal entry here and input your working hours
+                        </p>
+                    </div>
+                </div>
+                <div class="bg-white shadow-md rounded-md lg:h-48 flex flex-col items-center justify-center gap-6" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
+                    <div>
+                        <h1 class="font-semibold lg:text-2xl md:text-2xl sm:text-2xl ss:text-2xl xs:text-xl text-[13px] mt-5">Company Matches</h1>
+                    </div>
+                    <div>
+                        <button class="bg-[#AD974F] hover:bg-[#736023] text-white p-1 rounded-md lg:text-sm md:text-sm ss:text-sm sm:text-sm xs:text-sm text-[12px] lg:w-36 lg:mb-4 md:w-[9rem]  sm:w-[9rem]  ss:w-[9rem] w-[6rem]"><a href="{{ route('match-students') }}">Matched Companies</a></button>
+                    </div>
+                    <div>
+                        <p class="lg:text-sm md:text-[13px] sm:text-[12px] ss:text-[12px] xs:text-[10px] text-[10px] mb-2 text-center">Contact your coordinator for more information about your company matches
+                        </p>
                     </div>
                 </div>
             </div>
+
             @endforeach
             <!--  END OF THIRD ROW -->
         </div>
     </div>
 
-    <footer class="w-full bg-gray-800 relative bottom-0  h-[5rem]">
-        <div class="container mx-auto">
-            <div>
-                <img src="" alt="">
-            </div>
-        </div>
-    </footer>
+
+
+
 
     <script src="{{ asset('js/student/dashboard.js') }}"></script>
-
-
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init()
+    </script>
 </body>
+
 
 </html>
