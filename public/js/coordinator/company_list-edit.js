5a8cd6d135ea693d9d880d23f6247fa098d15e3f
JCYhapon <yhaponjc@gmail.com>
2024-03-19 21:59:00 +0800

    new script

--- public/js/coordinator/company_list-edit.js
+++ public/js/coordinator/company_list-edit.js
@@ -24,14 +24,17 @@ document
             newPositionItem.classList.add(
                 "position-item",
                 "flex",
+                "rounded-lg",
                 "items-center",
                 "mt-2",
-                "mr-2"
+                "mr-2",
+                "bg-gray-800"
             );
 
             // Create span element for position text
             const newPositionText = document.createElement("span");
             newPositionText.classList.add(
+                "position-text",
                 "rounded-lg",
                 "p-2.5",
                 "dark:placeholder-gray-400",
@@ -41,20 +44,21 @@ document
             newPositionText.textContent = customPosition;
 
             // Create remove button
-            const removeButton = document.createElement("input");
+            const removeButton = document.createElement("button");
             removeButton.type = "button"; // Set type to button
-            removeButton.value = "×"; // Set the value (content) of the button
             removeButton.classList.add(
                 "remove",
-                "pl-2",
-                "pr-1",
-                "cursor-pointer"
+                "pr-3.5",
+                "cursor-pointer",
+                "bg-gray-800",
+                "text-white"
             );
             removeButton.dataset.position = customPosition;
+            removeButton.innerHTML = "&times;"; // Display "×" symbol
 
             // Append elements to position item
             newPositionItem.appendChild(newPositionText);
-            newPositionText.appendChild(removeButton);
+            newPositionItem.appendChild(removeButton);
 
             // Append position item to container
             document
@@ -78,6 +82,7 @@ document
         }
     });
 
+// Event listener for adding predefined positions
 document
     .getElementById("addPosition")
     .addEventListener("change", function (event) {
@@ -95,37 +100,42 @@ document
             newPositionItem.classList.add(
                 "position-item",
                 "flex",
+                "rounded-lg",
                 "items-center",
                 "mt-2",
-                "mr-2"
+                "mr-2",
+                "bg-gray-800"
             );
 
             // Create span element for position text
             const newPositionText = document.createElement("span");
             newPositionText.classList.add(
+                "position-text",
                 "rounded-lg",
                 "p-2.5",
                 "dark:placeholder-gray-400",
-                "bg-gray-800",
-                "text-white"
+                "text-white",
+                "inline-block" // Add this class to display the position text inline
             );
             newPositionText.textContent = selectedPosition;
 
             // Create remove button
-            const removeButton = document.createElement("input");
+            const removeButton = document.createElement("button");
             removeButton.type = "button"; // Set type to button
-            removeButton.value = "×"; // Set the value (content) of the button
             removeButton.classList.add(
                 "remove",
-                "pl-2",
-                "pr-1",
-                "cursor-pointer"
+                "pr-3.5",
+                "cursor-pointer",
+                "bg-gray-800",
+                "text-white",
+                "inline-block" // Add this class to display the remove button inline
             );
             removeButton.dataset.position = selectedPosition;
+            removeButton.innerHTML = "&times;"; // Display "×" symbol
 
             // Append elements to position item
             newPositionItem.appendChild(newPositionText);
-            newPositionText.appendChild(removeButton);
+            newPositionItem.appendChild(removeButton);
 
             // Append position item to container
             document
@@ -137,9 +147,7 @@ document
             hiddenInput.type = "hidden";
             hiddenInput.name = "positions[]";
             hiddenInput.value = selectedPosition;
-            document
-                .querySelector(".position-container")
-                .appendChild(hiddenInput);
+            newPositionItem.appendChild(hiddenInput); // Append hidden input to newPositionItem
 
             // Clear selected option
             event.target.value = "";
@@ -153,111 +161,19 @@ document
         if (event.target.classList.contains("remove")) {
             event.preventDefault();
 
-            const matchedCompanyToRemove = event.target.dataset.position;
-            const matchedCompanyContainer = event.target.closest(
+            const matchedPositionToRemove = event.target.dataset.position;
+            const matchedPositionContainer = event.target.closest(
                 ".position-container"
             );
 
             event.target.closest(".position-item").remove();
 
             const hiddenInputsToRemove =
-                matchedCompanyContainer.querySelectorAll(
-                    'input[value="' + matchedCompanyToRemove + '"]'
+                matchedPositionContainer.querySelectorAll(
+                    'input[value="' + matchedPositionToRemove + '"]'
                 );
             hiddenInputsToRemove.forEach((input) => {
                 input.remove();
             });
         }
     });
-
-
-      document
-          .getElementById("addPosition")
-          .addEventListener("change", function (event) {
-              const selectedPosition = event.target.value;
-
-              if (selectedPosition) {
-                  // Remove the selected option from the dropdown list
-                  event.target
-                      .querySelector(`option[value="${selectedPosition}"]`)
-                      .remove();
-
-                  // Create a new position item
-                  const newPositionItem = document.createElement("div");
-                  newPositionItem.classList.add(
-                      "position-item",
-                      "flex",
-                      "items-center",
-                      "mt-2",
-                      "mr-2"
-                  );
-
-                  // Create span element for position text
-                  const newPositionText = document.createElement("span");
-                  newPositionText.classList.add(
-                      "rounded-lg",
-                      "p-2.5",
-                      "dark:placeholder-gray-400",
-                      "bg-blue-500",
-                      "text-white"
-                  );
-                  newPositionText.textContent = selectedPosition;
-
-                  // Create remove button
-                  const removeButton = document.createElement("input");
-                  removeButton.type = "button"; // Set type to button
-                  removeButton.value = "×"; // Set the value (content) of the button
-                  removeButton.classList.add(
-                      "remove",
-                      "pl-2",
-                      "pr-1",
-                      "cursor-pointer"
-                  );
-                  removeButton.dataset.position = selectedPosition;
-
-                  // Append elements to position item
-                  newPositionItem.appendChild(newPositionText);
-                  newPositionText.appendChild(removeButton);
-
-                  // Append position item to container
-                  document
-                      .querySelector(".position-container")
-                      .appendChild(newPositionItem);
-
-                  // Create hidden input field to store position
-                  const hiddenInput = document.createElement("input");
-                  hiddenInput.type = "hidden";
-                  hiddenInput.name = "positions[]";
-                  hiddenInput.value = selectedPosition;
-                  document
-                      .querySelector(".position-container")
-                      .appendChild(hiddenInput);
-
-                  // Clear selected option
-                  event.target.value = "";
-              }
-          });
-
-      // Delegate the event handling to the document level for remove buttons
-      document
-          .querySelector(".position-container")
-          .addEventListener("click", function (event) {
-              if (event.target.classList.contains("remove")) {
-                  event.preventDefault();
-
-                  const matchedCompanyToRemove = event.target.dataset.position;
-                  const matchedCompanyContainer = event.target.closest(
-                      ".position-container"
-                  );
-
-                  event.target.closest(".position-item").remove();
-
-                  const hiddenInputsToRemove =
-                      matchedCompanyContainer.querySelectorAll(
-                          'input[value="' + matchedCompanyToRemove + '"]'
-                      );
-                  hiddenInputsToRemove.forEach((input) => {
-                      input.remove();
-                  });
-              }
-          });
