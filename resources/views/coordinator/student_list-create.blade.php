9392e2417ced131e39a3241063e8c87966d0bc32
JCYhapon <yhaponjc@gmail.com>
2024-03-30 12:56:38 +0800

    button for creating student is justify right

--- resources/views/coordinator/student_list-create.blade.php
+++ resources/views/coordinator/student_list-create.blade.php
@@ -104,12 +104,15 @@
                         </div>
 
                     </div>
-                    <button type="submit" class="text-white inline-flex items-center bg-yellow-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                        </svg>
-                        Add student
-                    </button>
+                    <div class="flex justify-end">
+                        <button type="submit" class="text-white flex justify-end  bg-yellow-500  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                            </svg>
+                            Add student
+                        </button>
+                    </div>
+                   
                 </form>
             </div>
         </div>
